602c97f5a40d79d1c90bbe6d77a70936f1853258
Lauryn Gore <lauryngore@gmail.com>
2023-03-28 09:22:56 +0100

    added routing for movies and movie reviews

---
+++ movies-api/api/genres/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { genres } from "./genresData.js";
+import uniqid from "uniqid";
+
+const router = express.Router();
+
+// Get genres details
+router.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (genres.id == id) {
+    res.status(200).json(genres);
+  } else {
+    res.status(404).json({
+      message: "The resource you requested could not be found.",
+      status_code: 404,
+    });
+  }
+});
+
+export default router;
