a7b0e7ad2dfca4392b2d5039e1f540d010268637
Lauryn Gore <lauryngore@gmail.com>
2023-04-25 09:55:39 +0100

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,9 @@ import moviesRouter from "./api/movies";
 import './db';
 import './seedData'
 
+//... other imports
+import usersRouter from './api/users';
+
 dotenv.config();
 
 const app = express();
@@ -17,3 +20,7 @@ app.use("/api/movies", moviesRouter);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
+
+//Users router
+app.use('/api/users', usersRouter);
+
